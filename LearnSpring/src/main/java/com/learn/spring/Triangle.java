9d6827e0accf4eadc9434d5d13075841cd1273fc
Srinivasa Kumar Meda <sree.meda@gmail.com>
2013-12-10 21:37:40 -0800

    Example Demonstrates Constructor Injection. Example # 3

--- LearnSpring/src/main/java/com/learn/spring/Triangle.java
+++ LearnSpring/src/main/java/com/learn/spring/Triangle.java
@@ -2,17 +2,50 @@ package com.learn.spring;
 
 public class Triangle {
 
+	public Triangle(){
+		
+	}
+	
+	// Added for Example 3
+	public Triangle(String type) {
+		this.type = type;
+	}
+	
+	public Triangle(String type, int height) {
+		this.type = type;
+		this.height = height;
+	}
+	
+	public Triangle(int height){
+		this.height = height;
+	}
+	
+	private int height;
+
+	public int getHeight() {
+		return height;
+	}
+
+	// Added till here for example 3
+
+	// Added For example 2
 	private String type;
 
 	public String getType() {
 		return type;
 	}
 
+	/*
+	 * //Commenting out for example 3 to demonstrate constructor injection
 	public void setType(String type) {
 		this.type = type;
-	}
-
+	}*/
+	//Added till her for example 2
+	
+	// Modified this for all the three examples.
 	public void draw() {
-		System.out.println("Drawing Triangle of type " + getType() + "!!");
+		//System.out.println("Drawing Triangle!!"); 
+		//System.out.println("Drawing Triangle of type " + getType() + "!!");
+		System.out.println("Drawing Triangle of type " + getType() + " with height " + getHeight() + "!!");
 	}
 }
---
+++ LearnSpring/src/main/java/com/learn/spring/example3/readme.txt
@@ -0,0 +1,8 @@
+In the previous example, we have seen pre setting the value for a member variable in the spring configuration.
+That was done with the help of a setter method. hence thats called setter injection.
+
+In this example, we are going to take a look at Constructor injection.
+
+If we have a constructor for our bean which takes the parameters. We can use that constructor to set
+the parameter values instead of using their corresponding setter methods.
+
